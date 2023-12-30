86fc6f204132c3ee78ffa0dc3a8f3d6660a85dde
bouligo <bouligo@proton.me>
2023-12-30 01:39:18 +0100

    Autologin for SDDM in vmware

--- vmware.sh
+++ vmware.sh
@@ -78,6 +78,26 @@ arch-chroot /mnt systemctl enable sddm
 arch-chroot /mnt systemctl enable NetworkManager
 arch-chroot /mnt systemctl enable vmtoolsd
 
+printf "${CYAN}[*] ${GREEN} Enabling autologin for user $2 in SDDM ${NC}\n"
+mkdir /mnt/etc/sddm.conf.d/
+cat <<EOT >> /mnt/etc/sddm.conf.d/kde_settings.conf
+[Autologin]
+Relogin=false
+Session=plasma
+User=$2
+
+[General]
+HaltCommand=/usr/bin/systemctl poweroff
+RebootCommand=/usr/bin/systemctl reboot
+
+[Theme]
+Current=breeze
+
+[Users]
+MaximumUid=60513
+MinimumUid=1000
+EOT
+
 printf "${CYAN}[*] ${GREEN} Done. To have french keyboard in SDDM, run this as root after reboot : localectl set-x11-keymap fr${NC}\n"
 
 
