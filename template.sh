e7b301296ed89b4ae9e40eb3b65c7da70a884dd2
bouligo <bouligo@proton.me>
2024-07-05 00:07:36 +0200

    partitionmanager + krdp

--- template.sh
+++ template.sh
@@ -56,7 +56,7 @@ exit
 # VMware
 pacstrap /mnt open-vm-tools xf86-input-vmmouse xf86-video-vmware mesa
 # KDE
-pacstrap /mnt plasma yakuake dolphin spectacle kate networkmanager ark gwenview kolourpaint
+pacstrap /mnt plasma yakuake dolphin spectacle kate networkmanager ark gwenview kolourpaint filelight dolphin-plugins kwalletmanager kcalc kcharselect kdialog krdc ktorrent okular partitionmanager krdp
 # KDE minimal? 
 pacstrap /mnt plasma-desktop sddm sddm-kcm konsole dolphin
 # extra
