753ec8acdcd48938a454ad1cf50f99f1976e402b
bouligo <bouligo@proton.me>
2024-07-30 16:58:21 +0200

    Replaced tlp with power-profiles-daemon

--- workstation.sh
+++ workstation.sh
@@ -61,7 +61,7 @@ pacstrap /mnt plasma yakuake dolphin spectacle kate networkmanager ark gwenview 
 ## KDE minimal? 
 #pacstrap /mnt plasma-desktop sddm sddm-kcm konsole dolphin
 # extra
-pacstrap /mnt keepassxc firefox unzip discord docker dos2unix audacity filezilla gimp gnome-sound-recorder grc libreoffice-still ncdu networkmanager-openvpn obs-studio p7zip reflector rsync signal-desktop tlp traceroute tree xclip zip vlc wget yt-dlp
+pacstrap /mnt keepassxc firefox unzip discord docker dos2unix audacity filezilla gimp gnome-sound-recorder grc libreoffice-still ncdu networkmanager-openvpn obs-studio p7zip reflector rsync signal-desktop power-profiles-daemon traceroute tree xclip zip vlc wget yt-dlp
 ## Gnome
 #pacstrap /mnt gnome gnome-software-packagekit-plugin networkmanager
 
